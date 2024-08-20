86abfb1f550ff6f3b0fc9044c1b1d35d9bc94b42
tim <tim@brightsidedeveloper.com>
2024-08-19 19:33:25 -0500

    Implement Generics in BrightBaseEdge

--- src/classes/BrightBase/BrightBaseEdge.ts
+++ src/classes/BrightBase/BrightBaseEdge.ts
@@ -1,14 +1,16 @@
 import brightBaseSingleton from './BrightBaseSingleton'
 
-export default class BrightBaseEdge {
+export default class BrightBaseEdge<T extends { [funcName: string]: Record<string, unknown> }> {
   private edge: ReturnType<typeof brightBaseSingleton.getSupabase>['functions']
 
   constructor() {
     this.edge = brightBaseSingleton.getSupabase().functions
   }
 
-  async callFunction(functionName: string, params: Record<string, any>) {
-    const { data, error } = await this.edge.invoke(functionName, params)
+  async invoke<K extends keyof T>(functionName: K, payload: Record<K, T[K]>) {
+    const { data, error } = await this.edge.invoke(functionName as string, {
+      body: payload,
+    })
 
     if (error) {
       throw new Error(error.message)
